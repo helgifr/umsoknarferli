ab3fbb4e7f592e974507e46ed11ba7fe2c6f416e
Ytreg <helgifreyr95@gmail.com>
2018-12-15 23:43:00 +0000

    Updates

---
+++ precache-manifest.40ed3e63f115d112823cf9c1d1c23bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5225d8d25b3309678194a6838f8098e2",
+    "url": "/umsoknarferli/static/media/homeText.5225d8d2.md"
+  },
+  {
+    "revision": "6a4c7c501c105e280e81",
+    "url": "/umsoknarferli/static/js/runtime~main.6a4c7c50.js"
+  },
+  {
+    "revision": "420075b11773d855ebee",
+    "url": "/umsoknarferli/static/js/main.420075b1.chunk.js"
+  },
+  {
+    "revision": "b863b19d2a682f5ccdb1",
+    "url": "/umsoknarferli/static/js/1.b863b19d.chunk.js"
+  },
+  {
+    "revision": "420075b11773d855ebee",
+    "url": "/umsoknarferli/static/css/main.3e00c6fc.chunk.css"
+  },
+  {
+    "revision": "b863b19d2a682f5ccdb1",
+    "url": "/umsoknarferli/static/css/1.3763efcd.chunk.css"
+  },
+  {
+    "revision": "8b2f4feec7a20e78ef988a27ddaf1631",
+    "url": "/umsoknarferli/index.html"
+  }
+];
---
+++ static/media/homeText.5225d8d2.md
@@ -0,0 +1,5 @@
+## Velkomin á Umsóknarferli
+
+Hér er hægt að setja inn einskonar umsóknir og hægt er að fylgjast með ferli hverrar umsóknar með diagram-i.
+Hægt er að velja tímabil á dagatalinu fyrir neðan diagram-ið og séð umsóknirnar sem voru á því tímabili.
+Til að nota vefsíðuna þarf að búa til aðgang með notendanafn, nafn, og lykilorði.
